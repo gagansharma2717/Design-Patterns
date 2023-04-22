ffa6d88da980ff171002dee2596aa27a3f943a56
Gagan Sharma <gagansharma2717@gmail.com>
2023-04-22 12:48:03 +0530

    first commit

---
+++ src/Strategy_Pattern/OffroadVehicle.java
@@ -0,0 +1,9 @@
+package Strategy_Pattern;
+
+import Strategy_Pattern.Strategy.SpecialDriveStrategy;
+
+public class OffroadVehicle extends Vehicle{
+   OffroadVehicle(){
+       super(new SpecialDriveStrategy());
+   }
+}
