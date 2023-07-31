63a64827d47fcd1ac580be674c4acc67f2ac9794
Gagan Sharma <gagansharma2717@gmail.com>
2023-07-31 17:23:36 +0530

    Changes

--- src/Strategy_Pattern/OffroadVehicle.java
+++
@@ -1,9 +0,0 @@
-package Strategy_Pattern;
-
-import Strategy_Pattern.Strategy.SpecialDriveStrategy;
-
-public class OffroadVehicle extends Vehicle{
-   OffroadVehicle(){
-       super(new SpecialDriveStrategy());
-   }
-}
